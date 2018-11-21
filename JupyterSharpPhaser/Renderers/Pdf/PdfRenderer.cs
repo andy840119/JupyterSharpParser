d7068e1c83dc4c443cf9281b534abad249e0fbdf
為什麼 <andy840119@gmail.com>
2018-11-21 13:17:56 +0800

    GG , still cannot use . Throw : System.InvalidCastException: 'Unable to cast object of type 'iTextSharp.text.pdf.draw.LineSeparator' to type 'iTextSharp.text.Chunk'.'

--- JupyterSharpPhaser/Renderers/Pdf/PdfRenderer.cs
+++ JupyterSharpPhaser/Renderers/Pdf/PdfRenderer.cs
@@ -1,4 +1,7 @@
-﻿using JupyterSharpPhaser.Renderers.Html;
+﻿using iTextSharp.text;
+using iTextSharp.text.html.simpleparser;
+using iTextSharp.text.pdf;
+using JupyterSharpPhaser.Renderers.Html;
 using JupyterSharpPhaser.Syntax;
 using SelectPdf;
 using System;
@@ -33,13 +36,17 @@ namespace JupyterSharpPhaser.Renderers.Pdf
             writer.Flush();
             string htmlString = writer.ToString();
 
+            /*
             //Conver to pdf
             HtmlToPdf convertor = _htmlToPdf ?? CreateDefauleHtmlToPdf();
-
             var pdfFile = convertor.ConvertHtmlString(htmlString);
-
             //save
             pdfFile.Save(_stream);
+            */
+
+            var pdf = GetPDF(htmlString);
+
+            _stream.Write(pdf, 0 ,pdf.Length);
 
             //return stream
             return _stream;
@@ -51,6 +58,38 @@ namespace JupyterSharpPhaser.Renderers.Pdf
             newConvertor.Options.MarginTop = 30;
             newConvertor.Options.MarginBottom = 30;
             return newConvertor;
+        }
+
+        public byte[] GetPDF(string pHTML) {
+            byte[] bPDF = null;
+
+            MemoryStream ms = new MemoryStream();
+            TextReader txtReader = new StringReader(pHTML);
+
+            // 1: create object of a itextsharp document class
+            Document doc = new Document(PageSize.A4, 25, 25, 25, 25);
+
+            // 2: we create a itextsharp pdfwriter that listens to the document and directs a XML-stream to a file
+            PdfWriter oPdfWriter = PdfWriter.GetInstance(doc, ms);
+
+            // 3: we create a worker parse the document
+            HtmlWorker htmlWorker = new HtmlWorker(doc);
+
+            // 4: we open document and start the worker on the document
+            doc.Open();
+            htmlWorker.StartDocument();
+
+            // 5: parse the html into the document
+            htmlWorker.Parse(txtReader);
+
+            // 6: close the document and the worker
+            htmlWorker.EndDocument();
+            htmlWorker.Close();
+            doc.Close();
+
+            bPDF = ms.ToArray();
+
+            return bPDF;
         }
     }
 }
