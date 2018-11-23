586f4e9093ee4b47f4ad55075ec3c61a225cc49e
為什麼 <andy840119@gmail.com>
2018-11-23 18:23:07 +0800

    add a option to preview online

--- JupyterSharpPhaser.Web/Controllers/JupyterController.cs
+++ JupyterSharpPhaser.Web/Controllers/JupyterController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using JupyterSharpPhaser.Renderers.Html;
 using JupyterSharpPhaser.Renderers.Json;
@@ -71,7 +72,7 @@ namespace JupyterSharpPhaser.Web.Controllers
                         htmlRenderer.Render(document);
                         writer.Flush();
                     }
-                    htmlText = System.Text.Encoding.UTF8.GetString(memoryStream.ToArray());
+                    htmlText = Encoding.UTF8.GetString(memoryStream.ToArray());
                 }
 
                 //return view
@@ -87,6 +88,36 @@ namespace JupyterSharpPhaser.Web.Controllers
                 ModelState.AddModelError("Error!","Error occurred while pharsing the file");
                 return RedirectToAction("Index");
             }
+        }
+
+        [HttpPost]
+        public IActionResult PreviewHtml(JupyterPreviewModel model)
+        {
+            var jupyterText = model.PreviewJson;
+            if (!string.IsNullOrEmpty(jupyterText))
+            {
+                //convert to document
+                var document = Jupyter.Parse(jupyterText);
+
+                string htmlText = null;
+                using (var memoryStream = new MemoryStream())
+                {
+                    using (var writer = new StreamWriter(memoryStream))
+                    {
+                        var htmlRenderer = new HtmlRenderer(writer)
+                        {
+                            RendererHeaderAndFooter = true
+                        };
+                        htmlRenderer.Render(document);
+                    }
+                    htmlText = Encoding.UTF8.GetString(memoryStream.ToArray());
+                }
+
+                //preview
+                return  Content(htmlText, "text/html", Encoding.UTF8);;
+            }
+
+            return RedirectToAction("Index");
         }
 
         [HttpPost]
